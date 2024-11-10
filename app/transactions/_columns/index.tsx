2c546ed2febd398d1ed2b0654983edc5274ca5aa
Anthony <ant.davi07@gmail.com>
2024-11-10 14:29:40 -0300

    feat: ensure tall labels of transactions

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,9 @@
 import { Transaction } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import TransactionTypeBadge from "../_components/badge-type"
+import { TRANSACTION_CATEGORY_LABELS, TRANSACTION_PAYMENT_METHOD_LABELS } from "@/app/_constants/transactions"
+import { Button } from "@/app/_components/ui/button"
+import { PencilIcon, TrashIcon } from "lucide-react"
 
 export const transactionColums: ColumnDef<Transaction>[] = [
     {
@@ -17,21 +20,44 @@ export const transactionColums: ColumnDef<Transaction>[] = [
     {
         accessorKey: "category",
         header: "Categoria",
+        cell: ({ row: { original: transaction } }) => TRANSACTION_CATEGORY_LABELS[transaction.category],
     },
     {
         accessorKey: "paymentMethod",
         header: "Método de Pagamento",
+        cell: ({ row: { original: transaction } }) => TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
     },
     {
         accessorKey: "date",
         header: "Data",
+        cell: ({ row: { original: transaction } }) => new Date(transaction.date).toLocaleDateString("pt-br", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+        }),
     },
     {
         accessorKey: "amount",
         header: "Valor",
+        cell: ({ row: { original: transaction } }) => new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        }).format(Number(transaction.amount)),
     },
     {
         accessorKey: "actions",
-        header: "",
+        header: "Ações",
+        cell: () => {
+            return (
+                <div className="space-x-1">
+                    <Button variant="ghost" size="icon" className="text-muted-foreground">
+                        <TrashIcon />
+                    </Button>
+                    <Button variant="ghost" size="icon" className="text-muted-foreground">
+                        <PencilIcon />
+                    </Button>
+                </div>
+            )
+        }
     },
 ]
